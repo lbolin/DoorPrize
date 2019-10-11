9c9b52baf10c3c48a826a8aa6212b213e460f4f3
qbrandt <qbrandt@users.noreply.github.com>
2019-10-11 11:58:18 -0500

    Restructuring and Racer Page

--- doorPrize.js
+++ doorPrize.js
@@ -5,10 +5,11 @@ document.querySelector("#cancelBtn").addEventListener("click", returnToSelection
 const NUMRACERS = 5;
 
 var names = [];
+var pageTitles = ["Happy Racers!","Contestants","Race","Winner"];
 
 function pickContestants () {
+    names = [];
     let inputNames = document.querySelector("#inputNames").value;
-    //console.log(names);
     inputNames = inputNames.split("\n");
     inputNames = inputNames.filter(function (inputNames) {
         return inputNames.trim() !== "";
@@ -29,8 +30,7 @@ function pickContestants () {
             names.push(inputNames[index]);
         }
         updatePage2Names();
-        document.querySelector("#Page1").classList.toggle("hidden");
-        document.querySelector("#Page2").classList.toggle("hidden");
+        changePage(1,2);
     }
 }
 
@@ -38,5 +38,24 @@ function updatePage2Names () {
     let racerNames = document.querySelectorAll(".racerName");
     for(let i = 0; i < NUMRACERS; i++) {
         racerNames[i].innerHTML = names[i];
+    }
+}
+
+function startRace() {
+    changePage(2,3);
+}
+
+function returnToSelection() {
+    changePage(2,1);
+}
+
+function changePage(from, to) {
+    if (from >= 1 && from <= 4 && to >= 1 && to <= 4) {
+        document.querySelector("#Page" + from).classList.toggle("hidden");
+        document.querySelector("#Page" + to).classList.toggle("hidden");
+        document.querySelector("#PageTitle").innerHTML = pageTitles[to - 1];
+    }
+    else {
+        console.error("Invalid argument for changePage");
     }
 }
